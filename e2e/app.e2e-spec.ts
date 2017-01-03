cc5bcaa78b691c33e76b92ef1a55795bfedcdec0
Clayton K. N. Passos <netstart@gmail.com>
2017-01-03 14:14:29 -0200

    Update app.e2e-spec.ts

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('meu-curso-refinado-angular App', function() {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works');
+    expect(page.getParagraphText()).toEqual('app works!');
   });
 });

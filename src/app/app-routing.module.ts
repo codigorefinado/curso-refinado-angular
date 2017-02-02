eea382f3d52b7cda7a6aee7065154a365f886a62
Clayton K. N. Passos <netstart@gmail.com>
2017-02-01 23:48:19 -0200

    update package.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,22 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forRoot([
-      {path: 'change-detection', loadChildren: 'app/change-detection/change-detection.module#ChangeDetectionModule'},
+      {
+        path: 'change-detection',
+        loadChildren: 'app/change-detection/change-detection.module#ChangeDetectionModule'
+      },
+      {
+        path: 'giphy-search-auto',
+        loadChildren: 'app/giphy/giphy-search/giphy-search-auto/giphy-search-auto.module#GiphySearchAutoModule'
+      },
+      {
+        path: 'giphy-search',
+        loadChildren: 'app/giphy/giphy-search/giphy-search.module#GiphySearchModule'
+      }
+
     ])
   ],
-  exports: [ RouterModule]
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule {
 
--- src/app/giphy/giphy-search/giphy-search-auto/giphy-search-auto-routing.module.ts
+++ src/app/giphy/giphy-search/giphy-search-auto/giphy-search-auto-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GiphySearchAutoComponent } from './giphy-search-auto.component';
 
-const routes: Routes = [ { path: 'giphy-search-auto', component: GiphySearchAutoComponent } ];
+const routes: Routes = [ { path: '', component: GiphySearchAutoComponent } ];
 
 @NgModule({
   imports: [

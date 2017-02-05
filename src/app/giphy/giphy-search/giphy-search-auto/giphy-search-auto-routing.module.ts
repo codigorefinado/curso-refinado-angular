ad7240d146aa90ab5e1481b4e36cfbe8b473085b
Clayton K. N. Passos <netstart@gmail.com>
2017-02-05 10:40:55 -0200

    merge conflict

--- src/app/giphy/giphy-search/giphy-search-auto/giphy-search-auto-routing.module.ts
+++ src/app/giphy/giphy-search/giphy-search-auto/giphy-search-auto-routing.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { GiphySearchAutoComponent } from './giphy-search-auto.component';
 
-<<<<<<< HEAD
-=======
 const routes: Routes = [ { path: '', component: GiphySearchAutoComponent } ];
->>>>>>> eea382f3d52b7cda7a6aee7065154a365f886a62
 
 @NgModule({
   imports: [
-    RouterModule.forChild([ { path: '', component: GiphySearchAutoComponent } ])
-  ],
-  exports: [ RouterModule ]
+    RouterModule.forChild(routes)
+  ]
 })
 export class GiphySearchAutoRoutingModule {
 }
